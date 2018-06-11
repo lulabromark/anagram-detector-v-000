9fc5e6ea43a7f4badf5f4c7db8b9c882daf541d2
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-11 03:43:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     possible_matches = array.collect { |word| word.split("")}
     possible_matches.each do |i|
       # binding.pry
-      if possible_matches[i].sort == word.sort
+      if possible_matches[i].sort == @word.sort
         matches << possible_matches[i]
       end
     end
