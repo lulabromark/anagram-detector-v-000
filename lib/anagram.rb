8637c676b7c8d2eb599100cd6d48ba84eeb54513
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-11 03:35:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,16 @@ class Anagram
   def initialize(word)
     @word = word
   end
+
+  def self.match(array)
+    matches = []
+    possible_matches = array.collect { |word| word.split("")}
+    possible_matches.each do |i|
+      if matches[i].sort == word.sort
+        matches << match[i]
+      end
+    end
+    end
+    matches
+  end
 end
